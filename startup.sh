70fcadf833db25b0a7a58e1ab9d2dec105efda5e
Albert Iradukunda <albert.iradukunda@yahoo.com>
2024-03-16 16:31:10 +0300

    startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-python3 ./corebackend/manage.py collectstatic --noinput
-python3 ./corebackend/manage.py makemigrations --noinput
-python3 ./corebackend/manage.py migrate 
-gunicorn --workers 2 corebackend.corebackend.wsgi
+python3 /corebackend/manage.py collectstatic --noinput
+python3 /corebackend/manage.py makemigrations --noinput
+python3 /corebackend/manage.py migrate 
+gunicorn --workers 2 /corebackend/corebackend.wsgi
