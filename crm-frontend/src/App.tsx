65b5f3134fea7fb7908edaf4a92c4983590101bd
Albert Iradukunda <albert.iradukunda@yahoo.com>
2024-03-16 21:36:01 +0300

    d

--- crm-frontend/src/App.tsx
+++ crm-frontend/src/App.tsx
@@ -28,7 +28,7 @@ import Footer from "./components/Footer";
 import NotFound from "./components/404";
 
 const dev = "http://127.0.0.1:8000/api";
-const prod = "/api";
+const prod = location.origin + "/api";
 
 
 axios.defaults.baseURL =location.hostname === "localhost" ? dev : prod
