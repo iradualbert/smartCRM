d8b2e447443250055acd17c3cb6454c434dd6de5
Albert Iradukunda <albert.iradukunda@yahoo.com>
2024-03-17 10:33:32 +0300

    d

--- startup.sh
+++ startup.sh
@@ -1,4 +1,1 @@
-python3 corebackend/manage.py collectstatic --noinput
-python3 corebackend/manage.py makemigrations --noinput
-python3 corebackend/manage.py migrate 
 python3 corebackend/manage.py runserver 0.0.0.0:8000
